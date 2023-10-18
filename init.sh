8d09223df3797fb12dfaa8943da526af2b01bbdb
Camille Drouin <camille.drouin.1@ulaval.ca>
2023-10-18 08:40:03 -0400

    Commit du script init.sh
    Script pour la création de la structure de dossiers/fichier et les liens symboliques pour les données

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#initialisation du dossier d'analyse pour Étude de cas 1
+
+# $1 = premier argument = endroit ou la structure de dossiers et fichiers sera créée
+
+# 1. Créer la structure de base
+touch $1/README.md
+mkdir $1/data/
+mkdir $1/analyses/
+mkdir $1/résultats/
+mkdir $1/scripts/
+
+# 2. Créer les liens symboliques pour les données
+ln -sf /home/public/EtudesCas1/raw_data/ $1/data/
+ln -sf /home/public/rna_seq/reference/ $1/data/
+
+# 3. Copier les adapteurs
+mkdir $1/data/adaptors/
+cp /home/public/rna_seq/demo/adaptors.fa $1/data/adaptors/adaptors.fa
